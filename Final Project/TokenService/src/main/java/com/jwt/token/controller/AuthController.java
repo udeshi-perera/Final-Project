2aea27262e9b7cd9d22803f9d9c656963626aa66
udeshiperera <budeshimperera@gmail.com>
2021-02-28 10:25:35 +0530

    token service

---
+++ Final Project/TokenService/src/main/java/com/jwt/token/controller/AuthController.java
@@ -0,0 +1,79 @@
+package com.jwt.token.controller;
+
+import com.jwt.token.model.User;
+import com.jwt.token.payload.request.LoginRequest;
+import com.jwt.token.payload.request.SignupRequest;
+import com.jwt.token.payload.response.JwtResponse;
+import com.jwt.token.payload.response.MessageResponse;
+import com.jwt.token.repository.UserRepository;
+import com.jwt.token.security.jwt.JwtUtils;
+import com.jwt.token.security.services.UserDetailsImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+	@Autowired
+    AuthenticationManager authenticationManager;
+
+	@Autowired
+	UserRepository userRepository;
+
+	@Autowired
+    PasswordEncoder encoder;
+
+	@Autowired
+	JwtUtils jwtUtils;
+
+	@PostMapping("/signin")
+	public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+
+		Authentication authentication = authenticationManager.authenticate(
+				new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		String jwt = jwtUtils.generateJwtToken(authentication);
+		
+		UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+
+		return ResponseEntity.ok(new JwtResponse(jwt,
+												 userDetails.getId(), 
+												 userDetails.getUsername(), 
+												 userDetails.getEmail()
+												 //roles
+		));
+	}
+
+	@PostMapping("/signup")
+	public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
+		if (userRepository.existsByUsername(signUpRequest.getUsername())) {
+			return ResponseEntity
+					.badRequest()
+					.body(new MessageResponse("Error: Username is already taken!"));
+		}
+
+		if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+			return ResponseEntity
+					.badRequest()
+					.body(new MessageResponse("Error: Email is already in use!"));
+		}
+
+		// Create new user's account
+		User user = new User(signUpRequest.getUsername(),
+							 signUpRequest.getEmail(),
+							 encoder.encode(signUpRequest.getPassword()));
+		userRepository.save(user);
+
+		return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+	}
+}
---
+++ Final Project/TokenService/src/main/java/com/jwt/token/payload/response/MessageResponse.java
@@ -0,0 +1,12 @@
+package com.jwt.token.payload.response;
+
+import lombok.Data;
+
+@Data
+public class MessageResponse {
+	private String message;
+
+	public MessageResponse(String message) {
+	    this.message = message;
+	  }
+}
